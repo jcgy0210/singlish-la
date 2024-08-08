d6fd937860bd226f31e67f3699899ffd6a836be9
Ninja2421 <joashchua24@gmail.com>
2024-08-08 11:00:50 +0800

    added a seed folder and a placeholder data to populate the the table for testing in the future

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,21 @@
+// Placeholder data
+
+const users = [
+    {
+      id: '214h465089a3289',
+      name: 'User',
+      email: 'user@nextmail.com',
+      password: '123456',
+    },
+  ];
+
+  const admins = [
+    {
+      id: '2r434bn51y9a3243',
+      name: 'Admin',
+      email: 'admin@nextmail.com',
+      password: 'abcdef',
+    },
+  ];  
+
+  export {users, admins};
