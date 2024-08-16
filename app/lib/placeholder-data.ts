ba06eab21e83d528841434b3a9ee833b0195d2b2
“Joe” <joedocs01@gmail.com>
2024-08-16 23:54:52 +0800

    Set up PostgreSQL database

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -2,7 +2,7 @@
 
 const users = [
     {
-      id: '214h465089a3289',
+      id: '8080d1c3-a324-481c-91f7-8a311ecc1c3a',
       name: 'User',
       email: 'user@nextmail.com',
       password: '123456',
@@ -11,11 +11,11 @@ const users = [
 
   const admins = [
     {
-      id: '2r434bn51y9a3243',
+      id: 'f8ce6b6e-0109-48bd-a662-40956f09dc8d',
       name: 'Admin',
       email: 'admin@nextmail.com',
       password: 'abcdef',
     },
   ];  
 
-  export {users, admins};
+  export { users, admins };
--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Log in',
+  title: 'Login',
 };
 
 export default function LoginPage () {
