9ae85c3bcad8b2928bf0fc2459af7ecf514e1c90
“Joe” <joedocs01@gmail.com>
2024-08-16 22:17:08 +0800

    added function Page() in each page.tsx

---
+++ app/(pages-user)/courses/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'SinglishLA - Courses',
+};
+
+export default async function Page() {
+    return(
+        <p>This is Courses Page.</p>
+    )
+}
---
+++ app/(pages-user)/vocab/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'SinglishLA - Vocab List',
+};
+
+export default async function Page() {
+    return(
+        <p>This is Vocab List Page.</p>
+    )
+}
