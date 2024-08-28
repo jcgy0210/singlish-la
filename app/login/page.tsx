00c760ddf9f8f3db74bf09f58f3e82817f60bc56
“Joe” <joedocs01@gmail.com>
2024-08-28 14:10:07 +0800

    auth - attempt 3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,38 +1,8 @@
 "use client";
 
-// import { useActionState } from 'react';
 import LoginForm from "../ui/login-form";
 
 export default function AuthPage() {
-  // const [isSignIn, setIsSignIn] = useState(true);
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [loginAs, setLoginAs] = useState("user");
-  // const router = useRouter(); // Initialize useRouter
-
-  // const handleLogin = (e: any) => {
-  //   e.preventDefault();
-  //   const dummyUserEmail = "dummyuser@example.com";
-  //   const dummyAdminEmail = "dummyadmin@example.com";
-  //   const dummyPassword = "123";
-
-  //   if (
-  //     (loginAs === "user" && email === dummyUserEmail && password === dummyPassword) 
-  //   ) {
-  //     alert(`Login successful as ${loginAs}!`);
-  //     router.push("/user/home"); 
-  //   } 
-  //   else if ( 
-  //     (loginAs === "admin" && email === dummyAdminEmail && password === dummyPassword) ) {
-  //       alert(`Login successful as ${loginAs}!`);
-  //       router.push("/admin/dashboard"); 
-  //   }
-  //     else {
-  //     alert("Invalid email or password. Please try again.");
-  //   }
-  // };
-
-  
   return (
     <main className="flex min-h-screen items-center justify-center p-4 md:p-8 bg-gray-100">
       <LoginForm />
